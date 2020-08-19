ff8156616b5264144aa1247681de5d02848df12d
Jesus Manteca <jesusmanteca@gmail.com>
2020-08-19 12:41:55 -0500

    added a preventDefault to the event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 // you can use the querySelector to look for something in the html and find it. If it's an id, use a .prefix, if it's a class, use the #prefix - then you can use a built-in property of the DOM element, like .textContent to find the content within the element. 
 
-// here we have button element, buttonEl that we found by going into the document with a querySelector that is looking for #save-task
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-console.dir(buttonEl);
+// the form took the place of the inital button we created because we want to control what happens on the form AND the button
+var formEl = document.querySelector("#task-form");
 
 // We're eventually adding a task to the list so we may as well grab the task list
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -17,17 +15,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var counter = 0
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
     // create an element li
     var taskItemEl = document.createElement("li");
     // have the newly created element li say "Hello"
-    taskItemEl.textContent = "Hello";
+    taskItemEl.textContent =  "Hello"
     // add the css to this by adding the class name too
     taskItemEl.className = "task-item";
     // to the taskToDoEl, we'll append the taskItem we just created
     tasksToDoEl.appendChild(taskItemEl)
+
   }
 
-buttonEl.addEventListener("click", createTaskHandler)
+formEl.addEventListener("submit", createTaskHandler)
 
 
 
